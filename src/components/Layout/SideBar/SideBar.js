c9b41248819e530ad8f070f7a850642e46f21a78
brandonrq506 <darkrahd@gmail.com>
2022-07-27 21:21:15 -0600

    Created sidebar components

---
+++ src/components/Layout/SideBar/SideBar.js
@@ -0,0 +1,11 @@
+import styles from './SideBar.module.css'
+
+const SideBar = () => {
+    return (
+        <section className='styles.sidebar' >
+
+        </section>
+    );
+}
+
+export default SideBar;
